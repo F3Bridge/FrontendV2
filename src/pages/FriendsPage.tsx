a0ff63e43342fb91e1f4ee38367f5525b896f4dc
uleeeeee <ule@hotmail.fi>
2022-04-23 21:27:41 +0200

    friends added

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -1,10 +1,20 @@
 import {FaDiscord} from "react-icons/fa"
 
+const Friends = [
+	{
+		id: 1,
+		discordhandle: "rane#123",
+		followed: "no",
+		donateButton: "xd"
+	}
+]
+
 export function FriendsPage() {
   return (
     <div className="w-full h-screen bg-gray-300 ">
 		<h2 className="font-bold pt-8 ml-6">Discord Friends</h2>
 		<div className="bg-blue-300 w-[400px] h-[400px] rounded-md">
+			{}
 
 		</div>
 
