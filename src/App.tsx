ee1be77b3156a76fc099bc2c93a46935496ba533
Piotr Adamczyk <xeno@xeno.yt>
2022-04-23 16:11:29 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { useEffect, useState } from "react";
 import { providers } from "ethers";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { FriendsPage } from "./pages/FriendsPage";
 
 const App = () => {
   const [web3Provider, setWeb3Provider] = useState();
@@ -18,7 +21,12 @@ const App = () => {
     getProvider();
   }, []);
 
-  return <div className="text-xl text-red-500">Test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/friends" element={<FriendsPage />} />
+    </Routes>
+  );
 };
 
 export default App;
