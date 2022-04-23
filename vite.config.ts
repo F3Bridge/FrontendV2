33fa40e0136ee6ab0c6dc5a4c31aab2d3d288182
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-04-23 14:28:19 +0200

    WalletConnect implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: [
+        "*.js",
+        "node_modules/**/*.js",
+        new RegExp("node_modules/.vite/.*js"),
+      ],
+    }),
+  ],
+});
