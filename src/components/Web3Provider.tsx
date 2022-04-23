b4f32ff8c6b907c14fce276821d0f76fb2101ab6
Piotr Adamczyk <xeno@xeno.yt>
2022-04-23 17:37:38 +0200

    Broken web3auth impl

--- src/components/Web3Provider.tsx
+++ src/components/Web3Provider.tsx
@@ -1,13 +1,5 @@
-import { CoinbaseWallet } from "@web3-react/coinbase-wallet";
-import {
-  useWeb3React,
-  Web3ReactHooks,
-  Web3ReactProvider,
-} from "@web3-react/core";
-import { MetaMask } from "@web3-react/metamask";
-import { Network } from "@web3-react/network";
+import { Web3ReactHooks, Web3ReactProvider } from "@web3-react/core";
 import type { Connector } from "@web3-react/types";
-import { WalletConnect } from "@web3-react/walletconnect";
 import { ReactNode } from "react";
 import {
   coinbaseWallet,
@@ -18,14 +10,13 @@ import {
   hooks as walletConnectHooks,
   walletConnect,
 } from "../connectors/walletConnect";
+import { hooks as web3authHooks, web3auth } from "../connectors/web3auth";
 
-const connectors: [
-  MetaMask | WalletConnect | CoinbaseWallet | Network,
-  Web3ReactHooks
-][] = [
+const connectors: [Connector, Web3ReactHooks][] = [
   [metaMask, metaMaskHooks],
   [walletConnect, walletConnectHooks],
   [coinbaseWallet, coinbaseWalletHooks],
+  [web3auth, web3authHooks],
 ];
 
 export default function Web3Provider({ children }: { children?: ReactNode }) {
